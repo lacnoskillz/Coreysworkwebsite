3affc6efcd1d973d4bf31af59afa6288255ee772
lacnoskillz <kaikane3457@hotmail.com>
2023-08-02 16:31:41 -0500

    got rid of develop folder. restyled navigation bar using bootstrap and hamburger menu

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import '../Nav/Nav.css';
+
+const Nav = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarCollapse">
+          {Auth.loggedIn() ? (
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/reviews">
+                  Reviews
+                </Link>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link">
+                  Hey there, {Auth.getProfile().data.username}!
+                </span>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-light" onClick={logout}>
+                  Logout
+                </button>
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/reviews">
+                  Reviews
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
+
+
