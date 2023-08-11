f04e26123274a318e53d267401a400d6d15e3254
lacnoskillz <kaikane3457@hotmail.com>
2023-08-11 17:35:10 -0500

    made modal centered. cleaned up code a little. deleted redundant functions

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -5,18 +5,13 @@ import Modal from 'react-bootstrap/Modal';
 
 
 function Card({imageUrl, Title, Description}) {
-  function handleclick (Title){
-    console.log(Title,"clicked")
-    handleShow()
-  }
   const [show, setShow] = useState(false);
-  
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
     <div className="cardcontainer">
     <div className='card m-1 p-1 text-center'>
-      <img className ="cardimage" src={imageUrl} onClick={() => handleclick(Title)} alt='Card displaying a service' />
+      <img className ="cardimage" src={imageUrl} onClick={() => handleShow()} alt='Card displaying a service' />
       <div className='card-title'>{Title}</div>
     </div>
     <div
@@ -25,7 +20,7 @@ function Card({imageUrl, Title, Description}) {
     >
     
     </div>
-    <Modal show={show} onHide={handleClose}>
+    <Modal centered show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{Title}</Modal.Title>
         </Modal.Header>
